1e286aedffc6f4cde42f60e517437704950f5877
Yıldıray Yorulmaz <yildiray.yorulmaz@hotmail.com>
2022-11-18 05:41:52 +0300

    [backend] feat: exception handling added for book endpoints

--- backend/services/books.js
+++ backend/services/books.js
@@ -1,16 +1,48 @@
+const { InvalidQueryError, APIError } = require("../utils/errors");
+const HttpStatusCode = require("../utils/httpStatusCode");
+const expectedQueryValidator = require("../utils/expectedQueryValidator");
 const Book = require("../models/Book");
+const httpStatusCode = require("../utils/httpStatusCode");
 
-const getBooks = async (req, res) => {
-  res.json(await Book.findAll());
+const getBooks = async (req, res, next) => {
+  try {
+    if (expectedQueryValidator(req.query, ["title", "author", "edition"])) {
+      throw new InvalidQueryError(req.originalUrl);
+    }
+
+    const { title, author, edition } = req.query;
+    let where = {};
+    if (title) where.title = title;
+    if (author) where.author = author;
+    if (edition) where.edition = edition;
+
+    books = await Book.findAll(where);
+
+    res.json(books);
+  } catch (err) {
+    next(err);
+  }
 };
 
-const getBooksById = async (req, res) => {
-  const id = req.params.id;
-  const book = await Book.findByPk(id);
-  if (!book) {
-    return res.status(404).json({ message: "Book not found!" });
+const getBooksById = async (req, res, next) => {
+  try {
+    if (expectedQueryValidator(req.query, [])) {
+      throw new InvalidQueryError(req.originalUrl);
+    }
+
+    const id = req.params.id;
+    const book = await Book.findByPk(id);
+
+    if (!book)
+      throw new APIError(
+        httpStatusCode.NOT_FOUND,
+        "Book not found!",
+        req.originalUrl
+      );
+    res.json(book);
+  } catch (err) {
+    next(err);
   }
-  res.json(book);
 };
 
 module.exports = { getBooks, getBooksById };
