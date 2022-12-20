b2a438527bc32895dd2da7563bef3540eedf432c
Yıldıray Yorulmaz <yildiray.yorulmaz@hotmail.com>
2022-12-20 16:20:15 +0300

    [backend] feat: users endpoint created

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -5,5 +5,7 @@ const userService = require("../services/users");
 const router = express.Router();
 
 router.get("/", userService.getUsers);
+router.get("/:id", userService.getUsersById);
+router.get("/:id/address", userService.getUsersByIdWithAddress);
 
 module.exports = router;
--- backend/services/users.js
+++ backend/services/users.js
@@ -1,7 +1,67 @@
 const User = require("../models/User");
+const Address = require("../models/Address");
 
-const getUsers = (req, res) => {
-  res.json(User.findAll());
+const { InvalidQueryError, NotFoundError } = require("../utils/errors");
+
+const getUsers = async (req, res, next) => {
+  try {
+    const { ...others } = req.query;
+    if (Object.keys(others).length != 0) {
+      throw new InvalidQueryError(req.originalUrl);
+    }
+    let where = {};
+    const users = await User.findAll({ where });
+    res.json(users);
+  } catch (err) {
+    next(err);
+  }
 };
 
-module.exports = { getUsers };
+const getUsersById = async (req, res, next) => {
+  try {
+    const { ...others } = req.query;
+
+    if (Object.keys(others).length != 0) {
+      throw new InvalidQueryError(req.originalUrl);
+    }
+
+    const id = req.params.id;
+    const user = await User.findByPk(id);
+
+    if (!user) {
+      throw new NotFoundError(req.originalUrl);
+    }
+
+    res.json(user);
+  } catch (err) {
+    next(err);
+  }
+};
+
+const getUsersByIdWithAddress = async (req, res, next) => {
+  try {
+    const { ...others } = req.query;
+
+    if (Object.keys(others).length != 0) {
+      throw new InvalidQueryError(req.originalUrl);
+    }
+
+    const id = req.params.id;
+    const user = await User.findByPk(id, {
+      include: {
+        model: Address,
+        as: "address",
+      },
+    });
+
+    if (!user) {
+      throw new NotFoundError(req.originalUrl);
+    }
+
+    res.json(user);
+  } catch (err) {
+    next(err);
+  }
+};
+
+module.exports = { getUsers, getUsersById, getUsersByIdWithAddress };
