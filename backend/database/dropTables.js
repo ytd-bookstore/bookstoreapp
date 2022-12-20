a3816ae2f10cb0c9280e2adaeb222eaa55536dcd
Yıldıray Yorulmaz <yildiray.yorulmaz@hotmail.com>
2022-12-20 20:12:46 +0300

    [backend] refactor: district model removed

--- backend/database/dropTables.js
+++ backend/database/dropTables.js
@@ -3,7 +3,6 @@ const Book = require("../models/Book");
 const BookGenre = require("../models/BookGenre");
 const Cart = require("../models/Cart");
 const CartBook = require("../models/CartBook");
-const District = require("../models/District");
 const Favorite = require("../models/Favorite");
 const Genre = require("../models/Genre");
 const Order = require("../models/Order");
@@ -11,7 +10,6 @@ const OrderBook = require("../models/OrderBook");
 const User = require("../models/User");
 
 module.exports.dropTables = async () => {
-  await District.drop();
   await OrderBook.drop();
   await CartBook.drop();
   await BookGenre.drop();
