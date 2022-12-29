fa3bac9a4703d74e44db19f0bb734f6850df6089
Batuhan Dindar <88533266+batuhandindar40@users.noreply.github.com>
2022-12-29 18:06:15 +0300

    [mobile] refactor: Auth api is added to constants

--- mobile/assets/constants/apiConstants.js
+++ mobile/assets/constants/apiConstants.js
@@ -1,5 +1,5 @@
 const apiConstants = {
-  api: "http://10.0.2.2:3000/api/",
+  auth_api: "http://10.0.2.2:3000/api/auth/",
   mobile_api: "http://10.0.2.2:3000/api/mobile/",
   tokenKey: "ytdToken",
 };
