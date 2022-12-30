130af52e16b8ae854583d25868d445d1cd16ff66
ilter <iltertilki@yahoo.com>
2022-12-30 17:45:48 +0300

    [backend] feat: updated test cases

--- backend/test/unit/book.test.js
+++ backend/test/unit/book.test.js
@@ -1,14 +1,21 @@
 const bookService = require("../../services/books");
 const migrate = require("../../database/migration");
 
-describe("search books", () => {
+describe("search books->Valid name", () => {
   it("should return the searched book", async () => {
     const response = await bookService.searchBooks("Prince");
     expect(response.length >= 1).toBe(true);
   });
 });
 
-describe("get books by id with genres", () => {
+describe("search books -> Invalid name", () => {
+  it("should return the searched book", async () => {
+    const response = await bookService.searchBooks("Princeee");
+    expect(response.length >= 1).toBe(true);
+  });
+});
+
+describe("get books by id with genres -> Valid Book ID", () => {
   it("should return the book with its genres", async () => {
     const response = await bookService.getBooksByIdWithGenres(2);
 
@@ -16,7 +23,23 @@ describe("get books by id with genres", () => {
   });
 });
 
-describe("update books", () => {
+describe("get books by id with genres -> Invalid Book ID", () => {
+  it("should return the book with its genres", async () => {
+    const response = await bookService.getBooksByIdWithGenres(-5);
+
+    expect(response.genres.length >= 0).toBe(true);
+  });
+});
+
+describe("get books by id with genres -> Non-existing Book Id", () => {
+  it("should return the book with its genres", async () => {
+    const response = await bookService.getBooksByIdWithGenres(1005);
+
+    expect(response.genres.length >= 0).toBe(true);
+  });
+});
+
+describe("update books -> Valid Book Information and book ID", () => {
   const newBook = {
     title: "Kucukprens",
     author: "Kucukprens",
@@ -52,6 +75,126 @@ describe("update books", () => {
 
   it("should return the updated book", async () => {
     const response = await bookService.updateBook(newBook.id, updatedBook);
+    expect(response.stock >= 14).toBe(true);
+  });
+});
+
+describe("update books -> Invalid Book Information", () => {
+  const newBook = {
+    title: "Kucukprens",
+    author: "Kucukprens",
+    price: 12,
+    description: "adsdad",
+    edition: "First",
+    format: "Hardcover",
+    page: 122,
+    rating: 3.64,
+    rating_count: 12333,
+    image_url: "",
+    stock: "empty",
+  };
+
+  const updatedBook = {
+    title: "Kucukprens",
+    author: "Kucukprens",
+    price: 12,
+    description: "adsdad",
+    edition: "First",
+    format: "Hardcover",
+    page: 122,
+    rating: 3.64,
+    rating_count: 12333,
+    image_url: "",
+    stock: 14,
+  };
+
+  beforeAll(async () => {
+    const bookResponse = await bookService.createBook(newBook);
+    newBook.id = bookResponse.id;
+  });
+
+  it("should return the updated book", async () => {
+    const response = await bookService.updateBook(newBook.id, updatedBook);
+    expect(response.stock >= 14).toBe(true);
+  });
+});
+
+describe("update books -> Invalid Book ID", () => {
+  const newBook = {
+    title: "Kucukprens",
+    author: "Kucukprens",
+    price: 12,
+    description: "adsdad",
+    edition: "First",
+    format: "Hardcover",
+    page: 122,
+    rating: 3.64,
+    rating_count: 12333,
+    image_url: "",
+    stock: 13,
+  };
+
+  const updatedBook = {
+    title: "Kucukprens",
+    author: "Kucukprens",
+    price: 12,
+    description: "adsdad",
+    edition: "First",
+    format: "Hardcover",
+    page: 122,
+    rating: 3.64,
+    rating_count: 12333,
+    image_url: "",
+    stock: 14,
+  };
+
+  beforeAll(async () => {
+    const bookResponse = await bookService.createBook(newBook);
+    newBook.id = bookResponse.id;
+  });
+
+  it("should return the updated book", async () => {
+    const response = await bookService.updateBook(-5, updatedBook);
+    expect(response.stock >= 14).toBe(true);
+  });
+});
+
+describe("update books -> Non-existing Book ID", () => {
+  const newBook = {
+    title: "Kucukprens",
+    author: "Kucukprens",
+    price: 12,
+    description: "adsdad",
+    edition: "First",
+    format: "Hardcover",
+    page: 122,
+    rating: 3.64,
+    rating_count: 12333,
+    image_url: "",
+    stock: 13,
+  };
+
+  const updatedBook = {
+    title: "Kucukprens",
+    author: "Kucukprens",
+    price: 12,
+    description: "adsdad",
+    edition: "First",
+    format: "Hardcover",
+    page: 122,
+    rating: 3.64,
+    rating_count: 12333,
+    image_url: "",
+    stock: 14,
+  };
+
+  beforeAll(async () => {
+    const bookResponse = await bookService.createBook(newBook);
+    newBook.id = bookResponse.id;
+  });
+
+  it("should return the updated book", async () => {
+    const response = await bookService.updateBook(1005, updatedBook);
     expect(response.stock >= 14).toBe(true);
   });
 });
--- backend/test/unit/cart.test.js
+++ backend/test/unit/cart.test.js
@@ -2,7 +2,7 @@ const userService = require("../../services/users");
 const cartService = require("../../services/carts");
 const migrate = require("../../database/migration");
 
-describe("add book to cart", () => {
+describe("add book to cart -> Valid User and Book ID", () => {
   const newUser = {
     name: "John",
     surname: "Doe",
@@ -26,7 +26,103 @@ describe("add book to cart", () => {
   });
 });
 
-describe("get cart of user with books", () => {
+describe("add book to cart -> Invalid User ID", () => {
+  const newUser = {
+    name: "John",
+    surname: "Doe",
+    email: "johndoe@gmail.com",
+    passwordHash: "28ghjd2xz5",
+    passwordSalt: "68nf9358jd",
+    is_admin: "false",
+  };
+
+  beforeAll(async () => {
+    const userResponse = await userService.createUser(newUser);
+    newUser.id = userResponse.id;
+    await cartService.addBookToCart(-5, 1);
+  });
+  afterAll(async () => {
+    await userService.deleteUser(newUser.id);
+  });
+  it("should return cart with a book in it", async () => {
+    const response = await cartService.getCartOfUserWithBooks(newUser.id);
+    expect(response.books.length >= 1).toBe(true);
+  });
+});
+
+describe("add book to cart -> Non-existing User ID", () => {
+  const newUser = {
+    name: "John",
+    surname: "Doe",
+    email: "johndoe@gmail.com",
+    passwordHash: "28ghjd2xz5",
+    passwordSalt: "68nf9358jd",
+    is_admin: "false",
+  };
+
+  beforeAll(async () => {
+    const userResponse = await userService.createUser(newUser);
+    newUser.id = userResponse.id;
+    await cartService.addBookToCart(100, 1);
+  });
+  afterAll(async () => {
+    await userService.deleteUser(newUser.id);
+  });
+  it("should return cart with a book in it", async () => {
+    const response = await cartService.getCartOfUserWithBooks(newUser.id);
+    expect(response.books.length >= 1).toBe(true);
+  });
+});
+
+describe("add book to cart -> Invalid Book ID", () => {
+  const newUser = {
+    name: "John",
+    surname: "Doe",
+    email: "johndoe@gmail.com",
+    passwordHash: "28ghjd2xz5",
+    passwordSalt: "68nf9358jd",
+    is_admin: "false",
+  };
+
+  beforeAll(async () => {
+    const userResponse = await userService.createUser(newUser);
+    newUser.id = userResponse.id;
+    await cartService.addBookToCart(newUser.id, -3);
+  });
+  afterAll(async () => {
+    await userService.deleteUser(newUser.id);
+  });
+  it("should return cart with a book in it", async () => {
+    const response = await cartService.getCartOfUserWithBooks(newUser.id);
+    expect(response.books.length >= 1).toBe(true);
+  });
+});
+
+describe("add book to cart -> Non-existing Book ID", () => {
+  const newUser = {
+    name: "John",
+    surname: "Doe",
+    email: "johndoe@gmail.com",
+    passwordHash: "28ghjd2xz5",
+    passwordSalt: "68nf9358jd",
+    is_admin: "false",
+  };
+
+  beforeAll(async () => {
+    const userResponse = await userService.createUser(newUser);
+    newUser.id = userResponse.id;
+    await cartService.addBookToCart(newUser.id, 1008);
+  });
+  afterAll(async () => {
+    await userService.deleteUser(newUser.id);
+  });
+  it("should return cart with a book in it", async () => {
+    const response = await cartService.getCartOfUserWithBooks(newUser.id);
+    expect(response.books.length >= 1).toBe(true);
+  });
+});
+
+describe("get cart of user with books -> Valid User Id and Non-empty Cart", () => {
   const newUser = {
     name: "John",
     surname: "Doe",
@@ -50,7 +146,55 @@ describe("get cart of user with books", () => {
   });
 });
 
-describe("remove book from cart", () => {
+describe("get cart of user with books -> Invalid User ID", () => {
+  const newUser = {
+    name: "John",
+    surname: "Doe",
+    email: "johndoe@gmail.com",
+    passwordHash: "28ghjd2xz5",
+    passwordSalt: "68nf9358jd",
+    is_admin: "false",
+  };
+
+  beforeAll(async () => {
+    const userResponse = await userService.createUser(newUser);
+    newUser.id = userResponse.id;
+    await cartService.addBookToCart(-8, 1);
+  });
+  afterAll(async () => {
+    await userService.deleteUser(newUser.id);
+  });
+  it("should return cart with a book in it", async () => {
+    const response = await cartService.getCartOfUserWithBooks(newUser.id);
+    expect(response.books.length >= 1).toBe(true);
+  });
+});
+
+describe("get cart of user with books -> Non-existing User ID", () => {
+  const newUser = {
+    name: "John",
+    surname: "Doe",
+    email: "johndoe@gmail.com",
+    passwordHash: "28ghjd2xz5",
+    passwordSalt: "68nf9358jd",
+    is_admin: "false",
+  };
+
+  beforeAll(async () => {
+    const userResponse = await userService.createUser(newUser);
+    newUser.id = userResponse.id;
+    await cartService.addBookToCart(80, 1);
+  });
+  afterAll(async () => {
+    await userService.deleteUser(newUser.id);
+  });
+  it("should return cart with a book in it", async () => {
+    const response = await cartService.getCartOfUserWithBooks(newUser.id);
+    expect(response.books.length >= 1).toBe(true);
+  });
+});
+
+describe("remove book from cart -> Valid User and Book ID and a cart that contains the book", () => {
   const newUser = {
     name: "John",
     surname: "Doe",
@@ -65,6 +209,106 @@ describe("remove book from cart", () => {
     newUser.id = userResponse.id;
     await cartService.addBookToCart(newUser.id, 1);
     await cartService.removeBookFromCart(newUser.id, 1);
+  });
+  afterAll(async () => {
+    await userService.deleteUser(newUser.id);
+  });
+  it("should return cart with no book in it", async () => {
+    const response = await cartService.getCartOfUserWithBooks(newUser.id);
+    expect(response.books.length >= 1).toBe(false);
+  });
+});
+
+describe("remove book from cart -> Invalid User ID", () => {
+  const newUser = {
+    name: "John",
+    surname: "Doe",
+    email: "johndoe@gmail.com",
+    passwordHash: "28ghjd2xz5",
+    passwordSalt: "68nf9358jd",
+    is_admin: "false",
+  };
+
+  beforeAll(async () => {
+    const userResponse = await userService.createUser(newUser);
+    newUser.id = userResponse.id;
+    await cartService.addBookToCart("sadsa", 1);
+    await cartService.removeBookFromCart(newUser.id, 1);
+  });
+  afterAll(async () => {
+    await userService.deleteUser(newUser.id);
+  });
+  it("should return cart with no book in it", async () => {
+    const response = await cartService.getCartOfUserWithBooks(newUser.id);
+    expect(response.books.length >= 1).toBe(false);
+  });
+});
+
+describe("remove book from cart -> Non-existing User ID", () => {
+  const newUser = {
+    name: "John",
+    surname: "Doe",
+    email: "johndoe@gmail.com",
+    passwordHash: "28ghjd2xz5",
+    passwordSalt: "68nf9358jd",
+    is_admin: "false",
+  };
+
+  beforeAll(async () => {
+    const userResponse = await userService.createUser(newUser);
+    newUser.id = userResponse.id;
+    await cartService.addBookToCart(500, 1);
+    await cartService.removeBookFromCart(newUser.id, 1);
+  });
+  afterAll(async () => {
+    await userService.deleteUser(newUser.id);
+  });
+  it("should return cart with no book in it", async () => {
+    const response = await cartService.getCartOfUserWithBooks(newUser.id);
+    expect(response.books.length >= 1).toBe(false);
+  });
+});
+
+describe("remove book from cart -> Invalid Book ID", () => {
+  const newUser = {
+    name: "John",
+    surname: "Doe",
+    email: "johndoe@gmail.com",
+    passwordHash: "28ghjd2xz5",
+    passwordSalt: "68nf9358jd",
+    is_admin: "false",
+  };
+
+  beforeAll(async () => {
+    const userResponse = await userService.createUser(newUser);
+    newUser.id = userResponse.id;
+    await cartService.addBookToCart(newUser.id, -7);
+    await cartService.removeBookFromCart(newUser.id, 1);
+  });
+  afterAll(async () => {
+    await userService.deleteUser(newUser.id);
+  });
+  it("should return cart with no book in it", async () => {
+    const response = await cartService.getCartOfUserWithBooks(newUser.id);
+    expect(response.books.length >= 1).toBe(false);
+  });
+});
+
+describe("remove book from cart -> Non existing Book ID", () => {
+  const newUser = {
+    name: "John",
+    surname: "Doe",
+    email: "johndoe@gmail.com",
+    passwordHash: "28ghjd2xz5",
+    passwordSalt: "68nf9358jd",
+    is_admin: "false",
+  };
+
+  beforeAll(async () => {
+    const userResponse = await userService.createUser(newUser);
+    newUser.id = userResponse.id;
+    await cartService.addBookToCart(newUser.id, 1);
+    await cartService.removeBookFromCart(newUser.id, 1008);
   });
   afterAll(async () => {
     await userService.deleteUser(newUser.id);
